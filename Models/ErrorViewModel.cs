382530038510c823bb6a81e8bb2ff8224f8a16a4
Benildo <benildobenito1@gmail.com>
2022-09-21 10:54:42 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eTicketsStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
